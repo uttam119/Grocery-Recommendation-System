59384f6db336a0215be546137a6e85b30b6a69c4
unknown <ngc2058@gmail.com>
2024-03-16 14:03:32 +0545

    local upload system

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,30 +1,18 @@
 const AWS = require('aws-sdk');
-
+const multer = require("multer");
 const keys = require('../config/keys');
+const path = require("path")
 
-exports.s3Upload = async image => {
-  let imageUrl = '';
-  let imageKey = '';
-
-  if (image) {
-    const s3bucket = new AWS.S3({
-      accessKeyId: keys.aws.accessKeyId,
-      secretAccessKey: keys.aws.secretAccessKey,
-      region: keys.aws.region
-    });
-
-    const params = {
-      Bucket: keys.aws.bucketName,
-      Key: image.originalname,
-      Body: image.buffer,
-      ContentType: image.mimetype
-    };
-
-    const s3Upload = await s3bucket.upload(params).promise();
-
-    imageUrl = s3Upload.Location;
-    imageKey = s3Upload.key;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public"))
+  },
+  dest: path.join(__dirname, "../public"),
+  filename: function (req, file, cb) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    cb(null, file.fieldname + '-' + Date.now() + '.' + extension)
   }
-
-  return { imageUrl, imageKey };
-};
+})
+const upload = multer({ storage })
+exports.upload = upload;
