251924f1812dc7a5efad88b7a76c8161d81538f0
unknown <ngc2058@gmail.com>
2024-03-21 16:20:08 +0545

    frequently bought added

--- client/app/containers/ProductPage/FrequentlyBought.js
+++ client/app/containers/ProductPage/FrequentlyBought.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../constants';
+import { Link } from 'react-router-dom';
 
 const FrequentlyBought = ({ slug }) => {
-    const [loading, setLoading] = useState(false);
     const [data, setData] = useState([])
 
     const fetchData = async () => {
@@ -16,71 +16,67 @@ const FrequentlyBought = ({ slug }) => {
         fetchData()
     }, [])
 
+    if (data.length === 0) return null
 
     return <div className='product-container'>
         <h2>Frequently Bought</h2>
-        {data.length > 0 && data.map(product, index => {
-            return <div key={index} className='mb-3 mb-md-0'>
-                <div className='product-container'>
-                    <div className='item-box'>
-                        <div className='add-wishlist-box'>
-                            <AddToWishList
-                                id={product._id}
-                                liked={product?.isLiked ?? false}
-                                enabled={authenticated}
-                                updateWishlist={updateWishlist}
-                                authenticated={authenticated}
-                            />
-                        </div>
+        <div className='product-list'>
 
-                        <div className='item-link'>
-                            <Link
-                                to={`/product/${product.slug}`}
-                                className='d-flex flex-column h-100'
-                            >
-                                <div className='item-image-container'>
-                                    <div className='item-image-box'>
-                                        <img
-                                            className='item-image'
-                                            src={`${product.imageUrl
-                                                ? product.imageUrl
-                                                : '/images/placeholder-image.png'
-                                                }`}
-                                        />
+
+            {data.length > 0 && data.map((product, index) => {
+                return <div key={index} className='mb-3 mb-md-0'>
+                    <div className='product-container'>
+                        <div className='item-box'>
+
+                            <div className='item-link'>
+                                <Link
+                                    to={`/product/${product.slug}`}
+                                    className='d-flex flex-column h-60'
+                                >
+                                    <div className='item-image-container'>
+                                        <div className='item-image-box'>
+                                            <img
+                                                className='item-image'
+                                                src={`${product.imageUrl
+                                                    ? product.imageUrl
+                                                    : '/images/placeholder-image.png'
+                                                    }`}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className='item-body'>
-                                    <div className='item-details p-3'>
-                                        <h1 className='item-name'>{product.name}</h1>
-                                        {product.brand && Object.keys(product.brand).length > 0 && (
-                                            <p className='by'>
-                                                By <span>{product.brand.name}</span>
+                                    <div className='item-body'>
+                                        <div className='item-details p-3'>
+                                            <h1 className='item-name'>{product.name}</h1>
+                                            {product.brand && Object.keys(product.brand).length > 0 && (
+                                                <p className='by'>
+                                                    By <span>{product.brand.name}</span>
+                                                </p>
+                                            )}
+                                            <p className='item-desc mb-0'>{product.description}</p>
+                                        </div>
+                                    </div>
+                                    <div className='d-flex flex-row justify-content-between align-items-center px-4 mb-2 item-footer'>
+                                        <p className='price mb-0'>Rs.{product.price}</p>
+                                        {product.totalReviews > 0 && (
+                                            <p className='mb-0'>
+                                                <span className='fs-16 fw-normal mr-1'>
+                                                    {parseFloat(product?.averageRating).toFixed(1)}
+                                                </span>
+                                                <span
+                                                    className={`fa fa-star ${product.totalReviews !== 0 ? 'checked' : ''
+                                                        }`}
+                                                    style={{ color: '#ffb302' }}
+                                                ></span>
                                             </p>
                                         )}
-                                        <p className='item-desc mb-0'>{product.description}</p>
                                     </div>
-                                </div>
-                                <div className='d-flex flex-row justify-content-between align-items-center px-4 mb-2 item-footer'>
-                                    <p className='price mb-0'>Rs.{product.price}</p>
-                                    {product.totalReviews > 0 && (
-                                        <p className='mb-0'>
-                                            <span className='fs-16 fw-normal mr-1'>
-                                                {parseFloat(product?.averageRating).toFixed(1)}
-                                            </span>
-                                            <span
-                                                className={`fa fa-star ${product.totalReviews !== 0 ? 'checked' : ''
-                                                    }`}
-                                                style={{ color: '#ffb302' }}
-                                            ></span>
-                                        </p>
-                                    )}
-                                </div>
-                            </Link>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        })}
+            })}
+        </div>
     </div>
 
 }
